b43d9eaa9fd869711abcb0db5e1cd94e9645ed62
roy <try.roy87@gmail.com>
2024-11-17 09:43:05 -0600

    setup commit

---
+++ src/Recommended/Recommended.js
@@ -0,0 +1,8 @@
+import './Recommended.css'
+function Recommended() {
+  return (
+    <div>Recommended</div>
+  )
+}
+
+export default  Recommended;
---
+++ src/db/data.js
@@ -0,0 +1,12 @@
+const data = [
+    {
+        img: '',
+        title: "Nike Air",
+        reviewer:"(123 movies)",
+        prevPrice: "$140,00",
+        newPrice: "200",
+        company:"Nike",
+        color: "white",
+        category: "sneakers"
+    }
+];
